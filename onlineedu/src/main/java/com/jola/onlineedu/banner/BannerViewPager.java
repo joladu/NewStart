304b79783b25d305ee5db6e525ad1da5c27b4ab7
jola <1842132770@qq.com>
2018-09-29 21:59:54 +0800

    9-29wan home

--- onlineedu/src/main/java/com/jola/onlineedu/banner/BannerViewPager.java
+++ onlineedu/src/main/java/com/jola/onlineedu/banner/BannerViewPager.java
@@ -14,6 +14,7 @@ import android.widget.FrameLayout;
 public class BannerViewPager extends FrameLayout implements ViewPager.OnPageChangeListener{
 
     public BannerViewPager(@NonNull Context context) {
+//        fakjfskasdjf
         super(context);
     }
 
